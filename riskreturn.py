1f5825974ded006a983a084c17b91ca9a2e3e2c9
Mansib Rahman <mansibrahman@Mac.lan>
2025-10-29 14:03:21 -0700

    moved risk/return functionality to jupyter notebook

--- riskreturn.py
+++
@@ -1,7 +0,0 @@
-import numpy as np 
-
-def risk_reward(prices):
-    returns = [((prices[i] - prices[i-1]) / prices[i-1]) for i in range(1, len(prices))]
-    risk = np.std(returns)
-    reward = np.mean(returns)
-    return risk / reward
